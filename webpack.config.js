282cb9ea63693779dac6460bc363465bafa4ad71
TatianaKaiuk <persik961243@gmail.com>
2023-04-07 12:49:27 +0300

    .

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// const path = require('path');
+
+// module.exports = {
+//     entry: {
+//         profile: './scr/profile/index.js',
+// dashboard: './scr/dashboard/index.js',
+//     },
+//     output: {
+//         filename: '[name].js',
+//         path: path.join(__dirname, 'build')
+//     },
+// };
