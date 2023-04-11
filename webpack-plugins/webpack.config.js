0295bbe34ac1f16acbd0556fb66ef6c9c446b826
TatianaKaiuk <persik961243@gmail.com>
2023-04-11 11:44:49 +0300

    .

--- webpack-plugins/webpack.config.js
+++ webpack-plugins/webpack.config.js
@@ -1,38 +1,55 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js', // файл на вход
-  output: {
-    filename: 'bundle.js', // главный файл в продакшен название
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/, // какие файлы нужно преобразовать webpack,    записывается регулярным выражением
-        use: ['style-loader', 'css-loader', 'sass-loader'], // с помощью каких лоадеров это делать
-      },
-      {
-        test: /.(jpg|png)$/, // для подгрузки картинки
-        use: [
-          {
-            loader: 'url-loader', // какие лоадеры нужны
-            options: {
-              limit: 8192, // лимит размера картинки, если привышает, то автом используеться лоадер file-loader, который также нужно установить
-              name: '[name],[ext]', // имя картинки
-              outputPath: 'images', // папка для сохранения
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.js', // файл на вход
+    output: {
+      filename: 'bundle.js', // главный файл в продакшен название
+    },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/, // какие файлы нужно преобразовать webpack,    записывается регулярным выражением
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ], // с помощью каких лоадеров это делать
+        },
+        {
+          test: /.(jpg|png)$/, // для подгрузки картинки
+          use: [
+            {
+              loader: 'url-loader', // какие лоадеры нужны
+              options: {
+                limit: 8192, // лимит размера картинки, если привышает, то автом используеться лоадер file-loader, который также нужно установить
+                name: '[name],[ext]', // имя картинки
+                outputPath: 'images', // папка для сохранения
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true
+    }
+  };
+  if(isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      })
+    );
+  }
+  return config;
 };
